eb79c86cfbf75fbf0d04ec91ecf1e9405cd55997
rishirhyspavan <surisettypavankumar999@gmail.com>
2021-10-16 16:57:50 +0530

    initial-commit

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,71 @@
+import React, { useRef, useState } from "react";
+import emailjs from "emailjs-com";
+import "./Contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+
+const Contact = () => {
+  const [done, setDone] = useState(false);
+  const formRef = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_qa3ktm6",
+        "template_hwqsvrc",
+        formRef.current,
+        "user_e02EKmb0qBMkNnnzVJnQd"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +91 7995533369
+            </div>
+            <div className="c-info-item">
+              <img src={Email} alt="" className="c-icon" />
+              pavankumarsurisetty999@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img src={Address} alt="" className="c-icon" />
+              Anakapalli, Visakhapatnam 531002 India
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What’s your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along. me.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" name="user_name" />
+            <input type="text" placeholder="Subject" name="user_subject" />
+            <input type="text" placeholder="Email" name="user_email" />
+            <textarea name="message" placeholder="Message" rows="5" />
+            <button>Submit</button>
+            <br /> {done && <h4 className="c-wish">Thank You...</h4>}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://assets.uigarage.net/content/uploads/2019/09/Depositphotos_56829111_l-2015-1024x769.jpg",
+    link: "http://highalertloots.in",
+  },
+  {
+    id: 2,
+    img: "https://www.pixeden.com/media/k2/galleries/1394/001-devices-presentation-app-screens-project-showcase-graphic-psd-mockup.jpg",
+    link: "http://highalertloots.in",
+  },
+  {
+    id: 3,
+    img: "https://www.pixeden.com/media/k2/galleries/1369/001-devices-presentation-app-screens-graphic-psd-mockup.jpg",
+    link: "http://highalertloots.in",
+  },
+  {
+    id: 4,
+    img: "https://www.pixeden.com/media/k2/galleries/1379/001-devices-presentation-app-screens-project-showcase-graphic-psd-mockup.jpg",
+    link: "http://highalertloots.in",
+  },
+  {
+    id: 5,
+    img: "https://elements-cover-images-0.imgix.net/444332f3-f42b-4281-9642-c67fcc3dfd22?auto=compress%2Cformat&fit=max&w=1370&s=6ba6a36b7701311721076bbe58b1680b",
+    link: "http://highalertloots.in",
+  },
+  {
+    id: 6,
+    img: "https://elements-cover-images-0.imgix.net/87c939eb-12d9-44a7-8c77-9a42779db4eb?auto=compress%2Cformat&fit=max&w=1370&s=cf05cfda38c5bdb25e754b2e2e2544b2",
+    link: "http://highalertloots.in",
+  },
+];
